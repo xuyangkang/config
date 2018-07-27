0e4ac6b9194212a3d91ca6ba9b2eff7d8cab52e3
Xuyang Kang <xuyangkang@gmail.com>
2018-07-28 04:27:14 +0900

    Add some text in bootstrap.org

--- emacs_config/init.el
+++ emacs_config/init.el
@@ -1,7 +1,17 @@
-;; This buffer is for text that is not saved, and for Lisp evaluation.
-;; To create a file, visit it with C-x C-f and enter text in its buffer.
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
 (require 'org)
+;; TODO: extract a "compile-if-updated" function
+;; TODO: put these functions in a namespace
 (org-babel-tangle-file (concat user-emacs-directory "bootstrap.org"))
 (load-file (concat user-emacs-directory "bootstrap.el"))
 (byte-compile-file (concat user-emacs-directory "bootstrap.el"))
